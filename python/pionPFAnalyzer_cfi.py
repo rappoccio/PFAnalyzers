b72b1f438da549eec4c8dc3ba0fb5288d1c438e0
Salvatore Rappoccio <rappoccio@gmail.com>
2014-10-29 18:22:04 +0100

    remaking HBHE clusters

--- python/pionPFAnalyzer_cfi.py
+++ python/pionPFAnalyzer_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 
 pionPFAnalyzer = cms.EDAnalyzer('PionPFAnalyzer',
                                 dRMin = cms.double( 0.1 ),
-                                jetSrc = cms.InputTag('ak4PFJetsCHS'),
-                                clusterSrc = cms.InputTag('particleFlowClusterHCAL'),
+                                jetSrc = cms.InputTag('ak5PFJets'),
+                                clusterSrc = cms.InputTag('particleFlowClusterHBHE'),
                                 genSrc = cms.InputTag('genParticles')
                                    )
--- test/pionPFAnalyzer_test_cfg.py
+++ test/pionPFAnalyzer_test_cfg.py
@@ -5,6 +5,8 @@ process = cms.Process("ANA")
 ## MessageLogger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
+
+
 #input stuff for Run/Lumi selection with the "JSON"-formatted files from the PVT group
 import FWCore.PythonUtilities.LumiList as LumiList
 
@@ -14,27 +16,24 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_1.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_2.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_3.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_4.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_5.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_6.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_7.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_8.root',
-    '/store/user/rappocc/SinglePiPt100/PFRecHitTimingNoPUStep3/140925_161155/0000/step3_9.root',
+        'file:/build/grimes/SLHC17_patch2+clean_shashlik_timing/step3_13839_aged_vHCalDepthClustering_noOOT.root'
                                 )
                                 )
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('pionPFAnalyzer.root')
+                                   fileName = cms.string('pionPFAnalyzer_step3_13839_aged_vHCalDepthClustering_noOOT.root')
                                    )
 
 
 
 process.load('Analysis.PFAnalyzers.pionPFAnalyzer_cfi')
+process.load('RecoParticleFlow/PFClusterProducer/particleFlowRecHitHBHE_cfi')
+process.load('RecoParticleFlow/PFClusterProducer/particleFlowClusterHBHE_cfi')
+
+process.clusters = cms.Sequence(process.particleFlowRecHitHBHE+process.particleFlowClusterHBHE)
 
 process.p = cms.Path(
+    process.clusters+
     process.pionPFAnalyzer
     )
 
